fca3de57d62ac0f3cfa52788d51f9bc7b8d0cc32
Victoria Curl <victoriacurl9@gmail.com>
2020-01-31 14:17:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,12 +26,12 @@ class Artist
   def self.all
     @@all
      end
-   
+
      def self.find_or_create_by_name(name)
        self.all.detect {|artist| artist.name == name} || Artist.new(name).save
      end
-   
+
      def print_songs
-       self.songs.each {|song| puts song.name} 
+       self.songs.each {|song| puts song.name}
       end
    end
