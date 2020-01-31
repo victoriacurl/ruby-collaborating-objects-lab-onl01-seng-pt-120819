a32f26f0b58ec77f67b58b0d13b2473022d77246
Victoria Curl <victoriacurl9@gmail.com>
2020-01-31 14:57:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,39 +1,37 @@
 class Artist
+  attr_accessor :name
 
-  attr_accessor :name, :songs
-  @@all =[]
+  @@all = []
 
   def initialize(name)
     @name = name
-    @songs = []
+    @songs= []
   end
 
   def add_song(song)
     @songs << song
+    song.artist = self
+    song
+  end
+
+  def songs
+    @songs
   end
 
   def save
     @@all << self
+    self
   end
 
   def self.all
     @@all
   end
 
-  def self.find_or_create_by_name(artist_name)
-    found_artist = self.all.find {|artist| artist.name == artist_name}
-    if found_artist
-      found_artist
-    else
-      new_artist = self.new(artist_name)
-      new_artist.save
-      new_artist
-    end
+  def self.find_or_create_by_name(name)
+    self.all.detect {|artist| artist.name == name} || Artist.new(name).save
   end
 
   def print_songs
-    @songs.each do |song|
-      puts song.name
-    end
-  end
+    self.songs.each {|song| puts song.name} 
+   end
 end
